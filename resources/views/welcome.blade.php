ce32c3ee3b61527517ee010e4974ef89850fca8b
Barbara <barbarafialho@aluno.ifsp.edu.br>
2025-06-12 18:49:10 -0300

    alteração teste

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -862,7 +862,6 @@
             </div>
             <hr>
             <h1>Teste de deploy automático com GitHub Actions</h1>
-            <h2>Teste Teste</h2>
             <hr>
             <div class="mt-16">
                 <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
